c7db197b5d4ad928ce0f386f528d46ea58ea23ec
William Kent <wjk011@gmail.com>
2019-10-20 16:40:35 -0400

    Add products to package definition

--- Package.swift
+++ Package.swift
@@ -3,6 +3,11 @@ import PackageDescription
 
 let package = Package(
 	name: "SwiftUtilities",
+	products: [
+		.library(name: "SwiftKVO", targets: ["SwiftKVO"]),
+		.library(name: "LocalizedString", targets: ["LocalizedString"]),
+		.library(name: "CommandLineExtensions", targets: ["CommandLineExtensions"]),
+	],
 	targets: [
 		.target(name: "SwiftKVO"),
 		.target(name: "LocalizedString"),
