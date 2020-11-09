8f8bb86fee33410df3afafc1b3fa466be156fc79
William Kent <wjk011@gmail.com>
2020-11-09 13:43:45 -0500

    Remove SwiftKVO
    Combine should now be used instead.

--- Package.swift
+++ Package.swift
@@ -4,14 +4,12 @@ import PackageDescription
 let package = Package(
 	name: "SwiftUtilities",
 	products: [
-		.library(name: "SwiftKVO", targets: ["SwiftKVO"]),
 		.library(name: "LocalizedString", targets: ["LocalizedString"]),
 		.library(name: "CommandLineExtensions", targets: ["CommandLineExtensions"]),
 		.library(name: "AuthorizationAPI", targets: ["AuthorizationAPI"]),
 		.library(name: "CodesignCheck", targets: ["CodesignCheck"]),
 	],
 	targets: [
-		.target(name: "SwiftKVO"),
 		.target(name: "LocalizedString"),
 		.target(name: "CommandLineExtensions"),
 		.target(name: "AuthorizationAPI"),
