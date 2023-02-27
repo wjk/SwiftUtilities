78fd037e10873800577a425a8749089b5ca9411d
William Kent <wjk011@gmail.com>
2023-02-27 14:40:59 -0500

    Update LocalizedString library

--- Sources/LocalizedString/LocalizedString.swift
+++ Sources/LocalizedString/LocalizedString.swift
@@ -20,70 +20,78 @@
 
 import Foundation
 
+public func localize(_ string: String, table: String? = nil, bundle: Bundle? = nil) -> String {
+    let bundle = bundle ?? Bundle.main
+    return bundle.localizedString(forKey: string, value: nil, table: table)
+}
+
 public func localize(_ string: LocalizedStringBuilder, table: String? = nil, bundle: Bundle? = nil) -> String {
-	return string.resolve(tableName: table, bundle: bundle)
+    return string.resolve(tableName: table, bundle: bundle)
 }
 
 public struct LocalizedStringBuilder: ExpressibleByStringInterpolation {
-	public typealias StringLiteralType = String
-	fileprivate typealias InterpolatedValueProvider = () -> NSString
+    public typealias StringLiteralType = String
+    fileprivate typealias InterpolatedValueProvider = () -> NSString
 
-	private let key: String
-	private let valueProviders: [InterpolatedValueProvider]
+    private let key: String
+    private let values: [CVarArg]
 
-	public init(stringLiteral: String) {
-		self.key = stringLiteral
-		self.valueProviders = []
-	}
+    public init(stringLiteral: String) {
+        self.key = stringLiteral
+        self.values = []
+    }
 
-	public init(stringInterpolation: StringInterpolation) {
-		self.key = stringInterpolation.key
-		self.valueProviders = stringInterpolation.providers
-	}
+    public init(stringInterpolation: StringInterpolation) {
+        self.key = stringInterpolation.key
+        self.values = stringInterpolation.values
+    }
 
-	public func resolve(tableName table: String? = nil, bundle: Bundle? = nil) -> String {
-		let bundle = bundle ?? Bundle.main
+    public func resolve(tableName table: String? = nil, bundle: Bundle? = nil) -> String {
+        let bundle = bundle ?? Bundle.main
 
-		if valueProviders.count > 0 {
-			let arguments = valueProviders.map { provider in provider() }
-			let format = bundle.localizedString(forKey: self.key, value: nil, table: table)
-			return String(format: format, arguments: arguments)
-		} else {
-			return bundle.localizedString(forKey: self.key, value: nil, table: table)
-		}
-	}
+        if values.count > 0 {
+            let format = bundle.localizedString(forKey: self.key, value: nil, table: table)
+            return String(format: format, locale: Locale.current, arguments: values)
+        } else {
+            return bundle.localizedString(forKey: self.key, value: nil, table: table)
+        }
+    }
 
-	public struct StringInterpolation: StringInterpolationProtocol {
-		public typealias StringLiteralType = String
+    public struct StringInterpolation: StringInterpolationProtocol {
+        public typealias StringLiteralType = String
 
-		fileprivate var key = ""
-		fileprivate var providers: [InterpolatedValueProvider] = []
+        fileprivate var key = ""
+        fileprivate var values: [CVarArg] = []
 
-		public init(literalCapacity: Int, interpolationCount: Int) {
-		}
+        public init(literalCapacity: Int, interpolationCount: Int) {
+        }
 
-		public mutating func appendLiteral(_ literal: String) {
-			key += literal
-		}
+        public mutating func appendLiteral(_ literal: String) {
+            key += literal
+        }
 
-		public mutating func appendInterpolation(_ value: String) {
-			key += "%@"
-			providers.append { value as NSString }
-		}
+        public mutating func appendInterpolation(_ value: String) {
+            key += "%@"
+            values.append(value as NSString)
+        }
 
-		public mutating func appendInterpolation<Subject: CustomStringConvertible>(_ object: Subject) {
-			key += "%@"
-			providers.append { object.description as NSString }
-		}
+        public mutating func appendInterpolation(_ object: any CustomStringConvertible) {
+            key += "%@"
+            values.append(object.description as NSString)
+        }
 
-		public mutating func appendInterpolation<Subject>(_ object: Subject, formatter: Formatter) {
-			key += "%@"
-			providers.append {
-				guard let string = formatter.string(for: object) else {
-					fatalError("Could not convert object to string")
-				}
-				return string as NSString
-			}
-		}
-	}
+        public mutating func appendInterpolation<Subject>(_ object: Subject, formatter: Formatter) {
+            guard let string = formatter.string(for: object) else {
+                fatalError("Could not convert object to string")
+            }
+
+            key += "%@"
+            values.append(string as NSString)
+        }
+
+        public mutating func appendInterpolation(inflect value: Int) {
+            key += "%d"
+            values.append(value)
+        }
+    }
 }
